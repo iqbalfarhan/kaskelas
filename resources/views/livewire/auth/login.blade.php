f3c4ec6775dd11fc9086a15f02bdbfe579f532c2
Rohmat Hidayat <840002@telkom.co.id>
2023-10-31 14:37:57 +0800

    update build

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,10 +1,10 @@
 <div class="card bg-base-100 shadow w-full max-w-md mx-auto">
-    <form class="card-body p-14" wire:submit.prevent="login">
+    <form class="card-body lg:p-14" wire:submit.prevent="login">
         <img src="{{ url('YouFi.png') }}" alt="" class="w-48 mx-auto my-10">
         <div class="space-y-6 py-4">
             <div class="space-y-2">
-                <input type="text" class="input input-bordered p-6 w-full @error('username') input-error @enderror" wire:model="username" placeholder="Username" />
-                <input type="password" class="input input-bordered p-6 w-full @error('password') input-error @enderror" wire:model="password" placeholder="Password" />
+                <input type="text" class="input input-bordered w-full @error('username') input-error @enderror" wire:model="username" placeholder="Username" />
+                <input type="password" class="input input-bordered w-full @error('password') input-error @enderror" wire:model="password" placeholder="Password" />
                 <div class="form-control">
                     <label class="label cursor-pointer">
                         <span class="label-text">Remember me</span> 
