7e449b2f858dec4d77431a9c70528b03107dffeb
Rohmat Hidayat <840002@telkom.co.id>
2023-11-07 17:13:57 +0800

    update logo sekolah

--- database/seeders/SekolahSeeder.php
+++ database/seeders/SekolahSeeder.php
@@ -17,12 +17,12 @@ class SekolahSeeder extends Seeder
             [
                 "name" => "SMAN 1 Balikpapan",
                 "address" => "Jl. Marsma R. Iswahyudi, Sepinggan, Kecamatan Balikpapan Selatan, Kota Balikpapan, Kalimantan Timur 76115",
-                "logo" => "sekolah/VZZ48wOj0AknkiZNYeZrbf5Ki2Pim9XicNdC1MLA.png"
+                "logo" => null
             ],
             [
                 "name" => "MAN Balikpapan",
                 "address" => "JL. PRAPATAN NO. 10 PRAPATAN BALIKPAPAN KOTA",
-                "logo" => "sekolah/e0tJad9CdS9HSOc6NqvOaCORbONUaMidsGryZae1.png"
+                "logo" => null
             ]
         ];
 
