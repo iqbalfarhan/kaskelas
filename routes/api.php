f14bd4f77ea166dc1054910159f2e66a33007a93
IQBAL FARHAN SYUHADA <53584619+iqbalfarhan@users.noreply.github.com>
2023-11-10 20:30:37 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,7 @@ Route::get('/', [\App\Http\Controllers\ApiController::class, 'index'])->name('ap
 Route::get('/tele/tester', [\App\Http\Controllers\ApiController::class, 'teletester'])->name('tele.tester');
 Route::post('/telegram', [\App\Http\Controllers\ApiController::class, 'telegram'])->name('telegram');
 Route::get('/telegram/setWebhook', [\App\Http\Controllers\ApiController::class, 'updatewebhook']);
+
+Route::get('gitpull', function(){
+    shell_exec('cd /var/www/youfi && git pull');
+});
